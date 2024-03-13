0a260e85f4ba4d9e0efba960e5a1c41ac5eb8365
Alisher Izmukhan <alisher.izmukhan@nu.edu.kz>
2024-03-13 19:08:30 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,14 +1,8 @@
-from sqlalchemy import URL, create_engine
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-URL_DATABASE = URL.create(
-    'postgresql',
-    username='koyeb-adm',
-    password='yam3Cbgn4THq',
-    host='ep-sweet-sky-a2r1vkha.eu-central-1.pg.koyeb.app',
-    database='koyebdb',
-)
+URL_DATABASE = 'sqlite:///./data.db'
 
 engine = create_engine(URL_DATABASE)
 
